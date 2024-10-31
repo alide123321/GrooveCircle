b4b722e2827523b98e3bd4c21e74be77e6656a40
alide123321 <alide12332@gmail.com>
2024-10-31 11:48:10 -0400

    check user

--- Methods/GET/User.js
+++ Methods/GET/User.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const { database } = require('../../dbClient');
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+    
     const { userID } = req.query; 
 
-    if (!userID) 
+    if (!userID)
         return res.status(400).json({
             errmsg: "UserID is required"
         });
 
+    const users = database.collection('users');
+    const user = await users.findOne({"spotify_info.id" : userID });
+
     //calls other services to get user info and return it in the json
     
     res.status(200).json({
-        user: "userInfo"
+        user: user
     });
 });
 
