15455dff7238b615a1ba78cb502799602be1fcba
alide123321 <alide12332@gmail.com>
2024-11-21 12:00:37 -0500

    remove friend only removes from one side

--- Methods/User/DELETE/removefriend.js
+++ Methods/User/DELETE/removefriend.js
@@ -1,56 +1,49 @@
-const express = require("express");
-const fetch = require("node-fetch");
+const express = require('express');
+const fetch = require('node-fetch');
 const router = express.Router();
-const { database } = require("../../../dbClient");
+const { database } = require('../../../dbClient');
 
 // delete route for removing a friend
-router.delete("/", async (req, res) => {
-    const users = database.collection("users");
-    const { userid, friendid } = req.headers;
+router.delete('/', async (req, res) => {
+	const users = database.collection('users');
+	const { userid, friendid } = req.headers;
 
-    if (!userid || !friendid)
-        return res.status(400).send({
-            errmsg: "userid and friendid are required",
-        });
+	if (!userid || !friendid)
+		return res.status(400).send({
+			errmsg: 'userid and friendid are required',
+		});
 
-    if (userid === friendid)
-        return res.status(400).send({
-            errmsg: "User cannot add themselves as a friend",
-        });
+	if (userid === friendid)
+		return res.status(400).send({
+			errmsg: 'User cannot add themselves as a friend',
+		});
 
-    let fetchOptions = {
-        method: "GET",
-        headers: {
-        "Content-Type": "application/json",
-        userid: userid,
-        },
-    };
+	let fetchOptions = {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+			userid: userid,
+		},
+	};
 
-    fetch(`http://localhost:${process.env.PORT}/Friends`,fetchOptions)
-    .then((response) => response.json())
-    .then((body) => {
-      if (!body.friendsList) throw new Error("User not found");
+	let response = await fetch(`http://localhost:${process.env.PORT}/user`, fetchOptions);
+	if (response.status !== 200) {
+		return res.status(404).send({
+			errmsg: 'User not found',
+		});
+	}
 
-      users.updateOne({"spotify_info.id": userid}, {$pull: {friends_list: friendid}});
+	fetchOptions.headers.userid = friendid;
+	response = await fetch(`http://localhost:${process.env.PORT}/user`, fetchOptions);
+	if (response.status !== 200) {
+		return res.status(404).send({
+			errmsg: 'User not found',
+		});
+	}
 
-      fetchOptions.headers.userid = friendid;
+	await users.updateOne({ 'spotify_info.id': userid }, { $pull: { friends_list: friendid } });
 
-      fetch(`http://localhost:${process.env.PORT}/Friends`, fetchOptions)
-      .then((response) => response.json())
-      .then((body) => {
-        if (!body.friendsList) throw new Error("Friend not found");
-
-        users.updateOne({"spotify_info.id": friendid}, {$pull: {friends_list: userid}});
-      })
-    }).catch((error) => {
-      console.error("Error fetching user:", error);
-      return res.status(404).send({
-        errmsg: error.message || "An error occurred",
-      });
-    });
-
-  if (res.headersSent) return;
-  res.status(200).send(`User with ID \'${userid}\' removed \'${friendid}\' as a friend`);
+	res.status(200).send(`User with ID \'${userid}\' removed \'${friendid}\' as a friend`);
 });
 
 module.exports = router;
