0b97aeb78b0f6849abde1e7d821450abcd55bf94
alide123321 <alide12332@gmail.com>
2024-11-30 19:17:32 -0500

    check if user is already in a chatroom

---
+++ Methods/Messaging/GET/inChatroom.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { database } = require('../../../dbClient');
+const { ObjectId } = require('mongodb');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+	const { userid } = req.headers;
+
+	if (!userid) return res.status(400).json({ errmsg: 'Chatroom ID is required' });
+
+	const chatroomCollection = database.collection('chatrooms');
+	const chatroom = await chatroomCollection.findOne({ participants: { $in: [userid] } });
+
+	if (!chatroom._id) return res.status(404).json({ errmsg: 'User is not in a chatroom ' });
+
+	res.status(200).json({
+		messages: chatroom._id,
+	});
+});
+
+module.exports = router;
