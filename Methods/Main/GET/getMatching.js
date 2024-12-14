3751ab23e4cec7c1debdf9a531ecc575e34ea2ed
cyzhang <cyzhang002@gmail.com>
2024-12-14 00:02:06 -0500

    groove.html checks state of user upon loading

--- Methods/Main/GET/getMatching.js
+++ Methods/Main/GET/getMatching.js
@@ -20,9 +20,11 @@ router.get('/', async (req, res) => {
     if (!userinfo.ok) return res.status(404).send('issue with fetching user'); 
     userinfo = (await userinfo.json()).user; 
 
-    if (userinfo.isInQueue === false) return res.status(208).send('User is not in a queue'); 
+    if (userinfo.isInQueue === false) {
+        return res.status(208).json({ message: 'User is not in a queue', isInQueue: false }); 
+    }
 
-    res.status(200).send('User is in a queue'); 
+    res.status(200).json({ message: 'User is in a queue', isInQueue: true }); 
 }); 
 
 module.exports = router; 
